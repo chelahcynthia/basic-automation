663a0f71672ff9c9010d038f108f1c658bdb9dd1
chelahcynthia <cchelangat989@gmail.com>
2024-08-12 13:49:44 +0300

    Create json file

--- multiclipboard.py
+++ multiclipboard.py
@@ -2,3 +2,18 @@
 import sys
 import clipboard
 import json
+
+
+print(sys.argv)
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    
+    if command == "save":
+       print("save")
+    elif command == "load":
+       print("load")
+    elif command == "list":
+        print("Unknown command")
+else:
+    print("Please pass exactly one command")
