62f1f3787e81940f1be7f42e0437f508d9bb8c00
chelahcynthia <cchelangat989@gmail.com>
2024-08-15 10:15:03 +0300

    Create variable used to store my data

--- multiclipboard.py
+++ multiclipboard.py
@@ -3,23 +3,29 @@ import sys
 import clipboard
 import json
 
+SAVED_DATA = "clipboard.json"
+
 # create json
 def save_items(filepath, data):
    with open(filepath, "w") as f:
       json.dump(data, f)
       
-# function to read json file
-def load_json(filepath):
+# function to load json file
+def load_items(filepath):
    with open(filepath, "r") as f:
       data = json.load(f)
+      return data
 
       
    
 if len(sys.argv) == 2:
     command = sys.argv[1]
+    data = load_items(SAVED_DATA)
     
     if command == "save":
-       print("save")
+       key = input("Enter a key: ")
+       data[key] = clipboard.paste()
+       save_items(SAVED_DATA, data)
     elif command == "load":
        print("load")
     elif command == "list":
