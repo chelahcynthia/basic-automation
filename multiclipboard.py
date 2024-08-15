dc74b11c3b6af08b020449fb7e4d65b8b9deba21
chelahcynthia <cchelangat989@gmail.com>
2024-08-15 10:19:57 +0300

    Check if file exist, if not, return empty dictionary

--- multiclipboard.py
+++ multiclipboard.py
@@ -6,26 +6,30 @@ import json
 SAVED_DATA = "clipboard.json"
 
 # create json
-def save_items(filepath, data):
+def save_data(filepath, data):
    with open(filepath, "w") as f:
       json.dump(data, f)
       
 # function to load json file
-def load_items(filepath):
-   with open(filepath, "r") as f:
+def load_data(filepath):
+   try:
+      with open(filepath, "r") as f:
       data = json.load(f)
       return data
+   except:
+      return{}
+   
 
       
    
 if len(sys.argv) == 2:
     command = sys.argv[1]
-    data = load_items(SAVED_DATA)
+    data = load_data(SAVED_DATA)
     
     if command == "save":
        key = input("Enter a key: ")
        data[key] = clipboard.paste()
-       save_items(SAVED_DATA, data)
+       save_data(SAVED_DATA, data)
     elif command == "load":
        print("load")
     elif command == "list":
